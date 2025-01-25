29ccae3d746771a391ded0760192e8fb0dad6d50
anik475 <anikde475@gmail.com>
2025-01-25 05:30:37 -0500

    Implemented the skeleton of the Encoder Block for LLAMA2-7B

--- model.py
+++ model.py
@@ -71,6 +71,31 @@ class RMSNorm(nn.Module):
     def forward(self, x: torch.Tensor):
         # (dim) * (B, seq_len, dim) -> (B, seq_len, dim)
         return self.weight * self._norm(x.float()).type_as(x)
+    
+
+class EndcoderBlock(nn.Module):
+
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.head_dim = args.dim // args.n_heads
+
+        self.attention = SelfAttention(args)
+        self.feed_forward = FeedForward(args)
+
+        #Normalization before Attenstion
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+        #Normalization before Feed Forward
+        self.ff_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
+    def forward(self, x:torch.Tensor, start_pos: int, freqs_complex: torch.Tensor):
+        # (B, seq_len, dim) + (B, seq_len, dim) -> (B, seq_len, dim)
+        h = x + self.attention.forward(self.attention_norm(x), start_pos, freqs_complex)
+        out = h + self.feed_forward(self.ff_norm(h))
+        return out
+
 
 class Transformer(nn.Module):
     def __init__(self, args: ModelArgs) -> None:
